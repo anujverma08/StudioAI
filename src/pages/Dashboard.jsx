831ffb1ea12b15299b229f398a02b47208235c5e
Anuj Verma <anujvermagkp@gmail.com>
2025-08-06 19:19:57 +0530

    Iimplement CreationItems component to display individual creation details with expandable content

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyCreationData } from '../assets/assets';
+import { Sparkles, Gem } from 'lucide-react';
+import { Protect } from '@clerk/clerk-react';
 
+import CreationItems from '../components/CreationItems';
 const Dashboard = () => {
+  const [creation, setCreation] = useState([]);
+  const getDashboardData = async () => {
+    setCreation(dummyCreationData);
+  }
+
+  useEffect(() => {
+    getDashboardData()
+  }, []);
   return (
-    <div>
-      Dashboard
+    <div className='h-full overflow-y-scroll p-6'>
+      <div className='flex justify-start gap-4 flex-wrap'>
+        <div className='flex justify-between items-center w-72 p-4 bg-white rounded-xl border border-gray-200 shadow-sm'>
+          <div className='text-slate-600'>
+            <p className=' text-sm'>Total Creations</p>
+            <h2 className='text-xl font-semibold'>{creation.length}</h2>
+          </div>
+          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
+            <Sparkles className='w-5 text-white' />
+
+          </div>
+        </div>
+      {/* Active PLan card */}
+        <div className='flex justify-between items-center w-72 p-4 bg-white rounded-xl border border-gray-200 shadow-sm'>
+          <div className='text-slate-600'>
+            <p className=' text-sm'>Active Plan</p>
+            <h2 className='text-xl font-semibold'><Protect plan='premium_user' fallback="Free">Premium</Protect></h2>
+          </div>
+          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
+            <Gem className='w-5 text-white' />
+
+          </div>
+        </div>
+
+      </div>
+
+      <div className='space-y-3'>
+        <p className='mt-6 mb-4'>Recent Creations</p>
+        {
+          creation.map((item) => (
+            <CreationItems key={item.id} item={item} />
+          ))
+        }
+        <div>
+
+        </div>
+
+
+      </div>
+      
     </div>
   )
 }
